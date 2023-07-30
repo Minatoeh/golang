74b955a6ccfac9178b494cd73182476bfc76df3e
minatoeh <minatoeheh@gmail.com>
2023-07-30 09:50:36 +0000

    added post function. fix get func

--- getpost.go
+++ getpost.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -14,8 +15,8 @@ func TestGetBlogsEndpoint(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-
-	getBlogsHandler(rr, req)
+	handler := http.HandlerFunc(getBlogsHandler)
+	handler.ServeHTTP(rr, req)
 
 	if rr.Code != http.StatusOK {
 		t.Errorf("Oops! Something went wrong! Expected status 200, but got %d", rr.Code)
@@ -24,5 +25,29 @@ func TestGetBlogsEndpoint(t *testing.T) {
 	expectedResponse := `[{"title": "Blog ", "content": "Content "}]`
 	if rr.Body.String() != expectedResponse {
 		t.Errorf("Oops! Something went wrong! Expected response body %s, but got %s", expectedResponse, rr.Body.String())
+	}
+}
+
+func TestPostBlogEndpoint(t *testing.T) {
+	payload := map[string]string{
+		"title":   "Test Blog",
+		"content": "Test Content",
+	}
+	body, err := json.Marshal(payload)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", "/blogs", strings.NewReader(string(body)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(postBlogHandler)
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Oops! Something went wrong! Expected status 200, but got %d", rr.Code)
 	}
 }
